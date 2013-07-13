8fc1a9e05a9b34af664687b92a9f41a8ed4bdece
David Jones <drj@pobox.com>
2013-07-13 16:39:59 +0000

    Push into function.

--- zoopla.py
+++ zoopla.py
@@ -27,30 +27,37 @@ def subTagText(element, tag):
         pass
     return sub.text
 
-r = requests.get("http://api.zoopla.co.uk/api/v1/property_listings",
-  params=dict(api_key=ZOOPLA_KEY,
-    postcode=POSTCODE, radius=1.2,
-    property_type='houses',
-    listing_status='sale',
-    ordering='ascending',
-    page_size=100))
-print r.text
-root = etree.XML(r.text.encode('utf-8'))
-scraperwiki.sql.execute('drop table if exists House')
-scraperwiki.sql.execute('drop table if exists property')
-for e in root.xpath('/response/listing')[0]:
-  print e.tag
-for el in root.xpath('/response/listing'):
-  d = OrderedDict()
-  for tag in ['listing_id', 'price', 'image_url',
-    'displayable_address',
-    'details_url',
-    'latitude', 'longitude', 'short_description',
-    'num_bedrooms',
-    'num_bathrooms',
-    # 'num_floors', # Always 0
-    'num_recepts']:
+def propertyListings():
+    r = requests.get("http://api.zoopla.co.uk/api/v1/property_listings",
+      params=dict(api_key=ZOOPLA_KEY,
+        postcode=POSTCODE, radius=1.2,
+        property_type='houses',
+        listing_status='sale',
+        ordering='ascending',
+        page_size=100))
+    print r.text
+    root = etree.XML(r.text.encode('utf-8'))
+    scraperwiki.sql.execute('drop table if exists House')
+    scraperwiki.sql.execute('drop table if exists property')
+    for e in root.xpath('/response/listing')[0]:
+      print e.tag
+    for el in root.xpath('/response/listing'):
+      d = OrderedDict()
+      for tag in ['listing_id', 'price', 'image_url',
+        'displayable_address',
+        'details_url',
+        'latitude', 'longitude', 'short_description',
+        'num_bedrooms',
+        'num_bathrooms',
+        # 'num_floors', # Always 0
+        'num_recepts']:
 
-    d[tag] = subTagText(el, tag)
-  if d['listing_id']:
-    scraperwiki.sql.save(['listing_id'], d, table_name='House')
+        d[tag] = subTagText(el, tag)
+      if d['listing_id']:
+        scraperwiki.sql.save(['listing_id'], d, table_name='House')
+
+def main():
+    propertyListings()
+
+if __name__ == '__main__':
+    main()
